e0fd1a2c75947225db21c1089e11128d5bae615d
rimocon <ttyd4343-ttyd@yahoo.co.jp>
2017-12-19 16:31:34 +0900

    first commit

---
+++ eitango.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+//関数の定義
+void 
+void exit()
+int main(){
+	int choice=0;
+	printf("select mode\n");
+	printf("1--ZETTAI　TOEIC　QUESTION\n");
+	printf("2--ZETTAI　EIKEN　QUESTION\n");
+
