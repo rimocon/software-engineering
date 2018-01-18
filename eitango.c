b6058b355e486d42857e31e55e9533ff9718de9b
rimocon <ttyd4343-ttyd@yahoo.co.jp>
2018-01-18 12:09:29 +0900

    綺麗にした

--- eitango.c
+++ eitango.c
@@ -3,76 +3,60 @@
 #include<string.h>
 #include<stdlib.h>
 
-//関数の定義
+#define DATAMAX 30
 
 void toeic();
 void eiken();
 void exit();
 
-int main(){
-	int choice=0;
-	printf("select mode\n");
-	printf("1--ZETTAI　TOEIC　QUESTION\n");
-	printf("2--ZETTAI　EIKEN　QUESTION\n");
-	printf("3--exit\n");
-
-	scanf("%i",&choice);
-
-	switch(choice){
-
-		case 1:
-			toeic();
-			break;
-		case 2:
-			eiken();
-			break;
-			//case 3:
-			//	exit();
-			//	break;
-		default :
-			printf("無効な入力です\n");
-			break;
-	}
-}
-
-void toeic(){
-	int i;
-	FILE *fi,*fj,*fk;
+typedef struct{
 	char eibun[200];
-	char kotae [200];
+	char kotae[200];
 	char nihonbun[200];
-	char answer[200];
-	char score[40];
+	int  score;
+}record;
+
+record data[DATAMAX];
+
+int main(){
+
+	int i=0,score=0;
+	FILE *fi,*fj,*fk;
+	char answer[DATAMAX];
 
 	fi=fopen("toeic-eibun.txt","r");
 	fj=fopen("toeic-nihonbun.txt","r");
 	fk=fopen("toeic-kotae.txt","r");
 
-	while(fgets(eibun,200,fi)!=0 && fgets(nihonbun,200,fj)!=0 && fgets(kotae,200,fk)!=0){
-		printf("問題の英文:     %s",eibun);
-		printf("問題の日本語訳: %s",nihonbun);
-		printf("あなたの答え: ");
-		fgets(answer,200,stdin);
-		printf("解答: %s",kotae);
-		if(strcmp(answer,kotae)==0){
-			printf("正解\n");
-		}else{
-			printf("不正解\n");
-		}
-		
+	for(i=0;i<DATAMAX;i++){
+		fgets(data[i].eibun,200,fi);
+		fgets(data[i].nihonbun,200,fj);
+		fgets(data[i].kotae,200,fk);
 	}
 	fclose(fi);
 	fclose(fj);
 	fclose(fk);
 
+	for(i=0;i<DATAMAX;i++){
+		printf("%s",data[i].eibun);
+		printf("%s",data[i].nihonbun);
+		fgets(answer,DATAMAX,stdin);
+
+		if(strcmp(answer,data[i].kotae)==0){
+			printf("正解\n");
+			score++;
+
+		}else{
+			printf("不正解\n");
+		}
+		printf("答え: %s",data[i].kotae);
+	}
+	printf("正解数は %d / %d \n",score,DATAMAX);
+
+
 }
 
-void eiken(){
-	printf("sine\n");
-}
-//void exit(){
-//	printf("sine\n");
-//}
+
 
 
 
