81c5e7595492a11997be712bf7dc2f6c310cc89f
rimocon <ttyd4343-ttyd@yahoo.co.jp>
2018-01-25 11:42:16 +0900

    もう僕の仕事は終わり

--- eitango.c
+++ eitango.c
@@ -4,15 +4,14 @@
 #include<stdlib.h>
 
 #define DATAMAX 30
+#define MOJI 200
 
-void toeic();
-void eiken();
-void exit();
+
 
 typedef struct{
-	char eibun[200];
-	char kotae[200];
-	char nihonbun[200];
+	char eibun[MOJI];
+	char kotae[MOJI];
+	char nihonbun[MOJI];
 	int  score;
 }record;
 
@@ -23,15 +22,16 @@ int main(){
 	int i=0,score=0;
 	FILE *fi,*fj,*fk;
 	char answer[DATAMAX];
+	clock_t start,end;
 
 	fi=fopen("toeic-eibun.txt","r");
 	fj=fopen("toeic-nihonbun.txt","r");
 	fk=fopen("toeic-kotae.txt","r");
 
 	for(i=0;i<DATAMAX;i++){
-		fgets(data[i].eibun,200,fi);
-		fgets(data[i].nihonbun,200,fj);
-		fgets(data[i].kotae,200,fk);
+		fgets(data[i].eibun,MOJI,fi);
+		fgets(data[i].nihonbun,MOJI,fj);
+		fgets(data[i].kotae,MOJI,fk);
 	}
 	fclose(fi);
 	fclose(fj);
@@ -41,23 +41,13 @@ int main(){
 		printf("%s",data[i].eibun);
 		printf("%s",data[i].nihonbun);
 		fgets(answer,DATAMAX,stdin);
-
 		if(strcmp(answer,data[i].kotae)==0){
 			printf("正解\n");
 			score++;
-
 		}else{
 			printf("不正解\n");
 		}
 		printf("答え: %s",data[i].kotae);
 	}
 	printf("正解数は %d / %d \n",score,DATAMAX);
-
-
 }
-
-
-
-
-
-
